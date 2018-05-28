ecb1ec643a45246cc8bc22dac51ccc0b488dc6ca
raull44 <laxman.gautam44@gmail.com>
2018-05-28 10:38:59 +1000

    added some file

---
+++ src/Containers/Registration/Questionnaire.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Questionnaire extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      detail: [],
+      program: [],
+      injuryManagement: [],
+      stressAndProductivity: [],
+      healthAndWellbeing: [],
+      generalActivity: []
+    }
+  }
+  render() {
+    return(
+
+    )
+  }
+}
