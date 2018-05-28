56bb8b5c455fde8bc3a02be8d964a37a70a36b4f
raull44 <laxman.gautam44@gmail.com>
2018-05-28 12:09:38 +1000

    minor fixes

--- src/Containers/Registration/Questionnaire.js
+++ src/Containers/Registration/Questionnaire.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Progress from '../../Components/Registration/Questionnaire/Progress';
+import StepOne from '../../Components/Registration/Questionnaire/StepOne';
 
 class Questionnaire extends Component {
   constructor(props){
     super(props);
     this.state = {
       detail: {
-        name:"raul",
-        age:"28",
+        name:"Raul",
+        age:"",
         gender: "",
         currentBodyWeight: "",
       },
@@ -15,15 +16,64 @@ class Questionnaire extends Component {
       injuryManagement: [],
       stressAndProductivity: [],
       healthAndWellbeing: [],
-      generalActivity: []
+      generalActivity: [],
+      currentPage: 1
     }
   }
-  render() {
-    console.log(this.state);
-    return(
+  nextButtonHandler = () => {
+    console.log("Next button clicked");
+  }
+  previousButtonHandler = () => {
+    console.log("previouse button clicked");
+  }
 
-      <div><Progress />
-      We are in Questionnaire: {this.state.detail.name}</div>
+  detailHandler = () => {
+    console.log("DetailHander");
+  }
+
+  stepOneHandleChange = (propertyName, event) => {
+    console.log("handleChange", propertyName, event.target.value);
+    const detail = {...this.state.detail};
+    detail[propertyName] = event.target.value;
+    this.setState({ detail: detail});
+  }
+  // handleChange: function (propertyName, event) {
+  //    const contact = this.state.contact;
+  //    contact[propertyName] = event.target.value;
+  //    this.setState({ contact: contact });
+  //  },
+  //  render: function () {
+  //    return (
+  //      <div>
+  //        <input type="text" onChange={this.handleChange.bind(this, 'firstName')} value={this.state.contact.firstName}/>
+  //        <input type="text" onChange={this.handleChange.bind(this, 'lastName')} value={this.state.contact.lastName}/>
+  //        <input type="text" onChange={this.handleChange.bind(this, 'phone')} value={this.state.contact.lastName}/>
+  //      </div>
+  //    );
+  //  }
+
+  render() {
+
+    let RenderPage = null
+    if(this.state.currentPage === 1){
+      RenderPage = (
+          <div>
+            Name:<input type="text" onChange={this.stepOneHandleChange.bind(this, 'name')} value={this.state.detail.name}/>
+          </div>
+      );
+    } else if(this.state.currentPage === 2){
+      RenderPage = (
+          <Progress />
+      );
+    }
+
+    return(
+      <div>
+        <Progress currentPage={this.state.currentPage}/>
+        We are in Questionnaire: {this.state.detail.name}
+        {RenderPage}
+      </div>
+
 
     )
   }
