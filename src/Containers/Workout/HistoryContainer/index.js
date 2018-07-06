cd2e0e1ab029cae73e7027ce3d2351387eedb732
ckam028 <chaimkam@gmail.com>
2018-07-06 15:38:10 +1000

    connected plan and history component to footer

--- src/Containers/Workout/HistoryContainer/index.js
+++ src/Containers/Workout/HistoryContainer/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import FooterContainer from'../FooterContainer';
-import Plan from '../../../Components/Workout/Plan';
+import HistoryComponent from '../../../Components/Workout/History';
 
 export default class PlanContainer extends Component{
 
   render() {
     return (
       <div>
-      <Plan/>
+      <HistoryComponent/>
       <FooterContainer/>
       </div>
     )
