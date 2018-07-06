247166ce5ac3f941a31d552af382f21323faf0d4
ckam028 <chaimkam@gmail.com>
2018-07-06 17:46:25 +1000

    msg

--- src/Components/Workout/Plan/Content.js
+++ src/Components/Workout/Plan/Content.js
@@ -1,21 +1,43 @@
 import React, {Component} from 'react'
 import {List,Flex,Radio} from 'antd-mobile'
 
+const RadioItem = Radio.RadioItem
 const Item = List.Item
 
+
 export default class Content extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      WorkoutPlans:[
+        {day:'Day 1',workout:'Upperbody'},
+        {day:'Day 2',workout:'Abs'},
+        {day:'Day 3',workout:'Legs'},
+        {day:'Day 4',workout:'Cardio'},
+      ]
+    }
+  }
+
   render(){
     return(
-      <div>
-      <List className="my-list">
-      <Item style={{margin:"0 20px"}} arrow="horizontal" multipleLine onClick={() => console.log("it works")}>
-        <Flex>
-          <Flex.Item><Radio.RadioItem>Day 1</Radio.RadioItem></Flex.Item>
-          <Flex.Item>Workout</Flex.Item>
-        </Flex>
-      </Item>
-      </List>
-      </div>
+        <div>
+           <List className="my-list"> {this.state.WorkoutPlans.map(data => (
+              <div>
+                   <Flex>
+                      <Flex.Item>
+                          <Item arrow="horizontal">
+                          <RadioItem style={{marginRight:"50px"}}>
+                            <div style={{fontSize:'16px'}}>
+                              {data.day}       {data.workout}
+                              </div>
+                          </RadioItem>
+                          </Item>
+                      </Flex.Item>
+                   </Flex>
+             </div>
+             ))}
+           </List>
+        </div>
     )
   }
 }
