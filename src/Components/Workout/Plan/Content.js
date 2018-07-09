0c4b71fd8f9d9fcb9bf6d9f3bcbe1cc553b34516
ckam028 <chaimkam@gmail.com>
2018-07-09 15:43:37 +1000

    fixed bug on plan

--- src/Components/Workout/Plan/Content.js
+++ src/Components/Workout/Plan/Content.js
@@ -4,47 +4,29 @@ import {List,Flex,Radio} from 'antd-mobile'
 const RadioItem = Radio.RadioItem
 const Item = List.Item
 
-// two variations of contents, please comment out the one that is not used.
-
 export default class Content extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      /*WorkoutPlans:[
+      WorkoutPlans:[
         {day:'Day 1',workout:'Upperbody', value:1, isSelected:true},
         {day:'Day 2',workout:'Abs', value:2, isSelected:false},
-        {day:'Day 3',workout:'Legs', value:3,isSelected:false},
-        {day:'Day 4',workout:'Cardio', value:4,isSelected:false},
-      ],*/
-      WorkoutPlans2:[
-        {day:'Day 1',workout:'Upperbody', value:1, isSelected:true},
-        {day:'Day 2',workout:'Abs', value:2, isSelected:false},
-        {day:'Day 3',workout:'Legs', value:3,isSelected:false},
+        {day:'Day 3',workout:'Legs', value:32,isSelected:false},
         {day:'Day 4',workout:'Cardio', value:4,isSelected:false},
       ]
     }
   }
 
-  /*onChange = (value) => {
-    //write code to set default checked here so next time it will be checked by default.
 
+  onChange = (value) => {
     let temp = [ ...this.state.WorkoutPlans]
-    if(temp.length !== value){
-      temp[value].isSelected = true;
-      this.setState({WorkoutPlans:temp})
+    const index = this.state.WorkoutPlans.findIndex(i => { return i.value === value })
+    if(temp[index].isSelected === true && temp.length !== value){
+        temp[index+1].isSelected = true;
+        this.setState({WorkoutPlans:temp})
     }
 
-  }*/
-
-  onChange2 = (value) => {
-    //write code to set default checked here so next time it will be checked by default.
-
-    let temp = [ ...this.state.WorkoutPlans2]
-    if(temp.length !== value){
-      temp[value].isSelected = true;
-      this.setState({WorkoutPlans2:temp})
-    }
 
   }
 
@@ -52,33 +34,15 @@ export default class Content extends Component {
 
     return(
         <div>
-           {/*<List> {this.state.WorkoutPlans.map(data => (
-                   <Flex>
-                      <Flex.Item>
-                          <div>
-                          <Item arrow="horizontal">
-                          <RadioItem
-                            style={{marginRight:"50px"}}
-                            disabled={!data.isSelected}
-                            onChange={() => this.onChange(data.value)}
-                          >
-                            <div style={{fontSize:'16px'}}> {data.day} {data.workout} </div>
-                          </RadioItem>
-                          </Item>
-                          </div>
-                      </Flex.Item>
-                   </Flex>
-           ))}
-           </List>*/}
-           <List> {this.state.WorkoutPlans2.map(data2 => (
+           <List> {this.state.WorkoutPlans.map(data => (
            <Flex>
               <Flex.Item>
                   <div>
                   <Item
                     arrow="horizontal"
-                    disabled={!data2.isSelected}
-                    onClick={() => this.onChange2(data2.value)}>
-                    <div style={{fontSize:'16px'}}> {data2.day} {data2.workout} </div>
+                    disabled={!data.isSelected}
+                    onClick={() => this.onChange(data.value)}>
+                    <div style={{fontSize:'16px'}}> {data.day} {data.workout} </div>
                   </Item>
                   </div>
               </Flex.Item>
