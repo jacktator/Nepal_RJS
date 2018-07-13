8d07685ac5685832c0ba711eac44eb6410a2d0b1
raull44 <laxman.gautam44@gmail.com>
2018-07-13 22:27:59 +1000

    Workout when keep button pressed, keep and changed is replaced by saved

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux';
 import LoginDetailsReducers from '../Containers/LoginDetailsContainer/reducer';
 import QuestionnaireReducers from '../Containers/QuestionnaireContainer/reducer';
+import WorkOutReducers from '../Containers/Workout/WorkoutContainer/reducer';
 import SignUpReducers from '../Containers/SignUpContainer/reducer';
 import ForgetPasswordReducers from '../Containers/ForgetPasswordContainer/reducer';
 import RootReducers from '../Containers/RootContainer/reducer';
@@ -10,6 +11,7 @@ import FooterReducers from '../Containers/Workout/FooterContainer/reducer';
 //now Reducer represnts data union
 
 const Reducer = combineReducers({
+  WorkOutReducers,
   QuestionnaireReducers,
   FooterReducers,
   LoginDetailsStates: LoginDetailsReducers,
