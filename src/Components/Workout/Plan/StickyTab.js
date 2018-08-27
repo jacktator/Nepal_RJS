f8070a8d42dba8d9f818a2cfb07cc8699013035c
ckam028 <chaimkam@gmail.com>
2018-08-27 11:28:18 +1000

    fixing json

--- src/Components/Workout/Plan/StickyTab.js
+++ src/Components/Workout/Plan/StickyTab.js
@@ -6,16 +6,18 @@ export default class StickyTab extends React.Component {
 
   render() {
     const tabs = [
-     { title: 'WEEK 1' },
-     { title: 'WEEK 2' },
-     { title: 'WEEK 3' },
-     { title: 'WEEK 4' },
-     { title: 'WEEK 5' },
+     { title: 'WEEK 1',unlocked: true },
+     { title: 'WEEK 2',unlocked: false },
+     { title: 'WEEK 3',unlocked: false },
+     { title: 'WEEK 4',unlocked: false },
+     { title: 'WEEK 5',unlocked: false },
    ];
 
    return (
      <div className='plan-tabs'>
-        <Tabs tabs={tabs} renderTabBar={props => <Tabs.DefaultTabBar {...props} page={4}/>}>
+        <Tabs tabs={tabs} renderTabBar={
+              props => <Tabs.DefaultTabBar {...props} page={4}/>
+          }>
           <div className='content'>
              <Content/>
           </div>
