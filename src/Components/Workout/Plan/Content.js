9d96b41b1ed5659a8802abc1874ab20111af6468
Yupeili <474680647@qq.com>
2018-09-05 12:33:10 +0800

    fix sticktags bug

--- src/Components/Workout/Plan/Content.js
+++ src/Components/Workout/Plan/Content.js
@@ -8,6 +8,8 @@ export default class Content extends Component {
   handleClick(e, k) {
     if (!(this.props.selectedWeek < (this.props.WorkoutReducers.program.progress / this.props.WorkoutReducers.program.days))) {
       if (!(k < (this.props.WorkoutReducers.program.progress % this.props.WorkoutReducers.program.days))) {
+        if(this.props.WorkoutReducers.program.progress % this.props.WorkoutReducers.program.days === 0 ){return null}
+        console.log(k != this.props.WorkoutReducers.program.days)
         e.preventDefault();
       }
     }
@@ -26,7 +28,7 @@ export default class Content extends Component {
                 <Link key={k} to={'/workout/' + dayNumber} onClick={ event => this.handleClick(event, k) }>
                   <Item
                   arrow="horizontal"
-                  disabled={(this.props.selectedWeek)<(progress/days) ? false : (!(k < (progress % days)))}
+                  disabled={(this.props.selectedWeek)<(progress/days) ? false : progress%days === 0 ? false : (!(k < (progress % days)))}
                   style={{width:'100%'}}
                   // onClick={() => this.onChange(data.value)}
                   >
