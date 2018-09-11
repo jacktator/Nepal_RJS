f4ce0a1c9da8eed881428a2f3a592cde3a1f4567
hochungwong <chung940712@gmail.com>
2018-09-11 16:21:36 +1000

    Add actions and reducers in History Container

--- src/Containers/Workout/HistoryContainer/index.js
+++ src/Containers/Workout/HistoryContainer/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {selectFooter} from '../FooterContainer/actions';
+import {getProgram,getExerciseRecord,setDayIndex} from '../HistoryContainer/action';
 import FooterContainer from'../FooterContainer';
 
 import HistoryWeekly from '../../../Components/Workout/History/HistoryWeekly';
@@ -15,11 +16,11 @@ class PlanContainer extends Component{
     super(props);
     // console.log(this.props.match.path.substring(1))
     this.state = {
-      // history: [
-      //   {program:'push up', date:'2018/05/15', day:{one:{task: 'task' }, two: { task: 'task'} } },
-      //   {program:'pull up', date:'2018/05/15', day:{one:{task: 'task' }, two: { task: 'task'} } },
-      //   {program:'chin up', date:'2018/05/15', day:{one:{task: 'task' }, two: { task: 'task'} } },
-      // ],
+      history: [
+        {program:'push up', date:'2018/05/15', day:{one:{task: 'task' }, two: { task: 'task'} } },
+        {program:'pull up', date:'2018/05/15', day:{one:{task: 'task' }, two: { task: 'task'} } },
+        {program:'chin up', date:'2018/05/15', day:{one:{task: 'task' }, two: { task: 'task'} } },
+      ],
       currentPage: 1,
     }
   }
@@ -27,68 +28,59 @@ class PlanContainer extends Component{
     if(this.props.currentFooterTab!== 'historyTab' ){
       this.props.selectFooter('historyTab');
     }
+    this.props.getProgram();
+  }
+  componentDidMount(){
+    if(this.props.HistoryReducers){
+      this.props.getExerciseRecord();
+    }
   }
   onListProgramClickHandler = (e, program) => {
-    e.preventDefault();
-    // let currentHistoryIndex = this.state.history.findIndex( data =>{ return data.program === program});
-    this.setState({ currentPage: this.state.currentPage+1})
+      e.preventDefault();
+      // let currentHistoryIndex = this.state.history.findIndex( data =>{ return data.program === program});
+      this.setState({ currentPage: this.state.currentPage+1})
   }
-onParticularDayClickedHandler =(e, program ) => {
+  onParticularDayClickedHandler =(e, program ) => {
   e.preventDefault();
   this.setState({ currentPage: this.state.currentPage+1})
-}
+  }
 
   render() {
     let RenderPage = null;
-    console.log("this is from history",this.props.WorkoutReducers)
+    console.log("this is from history",this.props.HistoryReducers)
 
-    let {record,program} = this.props.WorkoutReducers;
 
-    let totalDays = parseInt((program.days),10);
-    let currentWeek = this.props.WorkoutReducers.currentWeek;
-    let currentDay = this.props.WorkoutReducers.currentDay;
-
-    let weekIndex = (record.weekly_record.findIndex(i => 
-      {return i.week === currentWeek.toString()}))
-    let weekNum = (record.weekly_record[weekIndex].week);//string
-
-    let dayIndex = (record.weekly_record[weekIndex].daily_record.findIndex(j => 
-      {return j.day === currentDay.toString()}))
-    let exerciseData = (record.weekly_record[weekIndex].daily_record[dayIndex].data)
-
-    const currentlyWeek = Week(program.progress,program.days)
-
-    if(this.state.currentPage === 1){
-      RenderPage = (
-        <HistoryComponent
-          onListProgramClick={this.onListProgramClickHandler}
-          WorkoutReducers= {this.props.WorkoutReducers}
-        />
-      )
-    }
-    if(this.state.currentPage === 2){
-      RenderPage = (
-        <HistoryWeekly 
-          onParticularDayClicked={this.onParticularDayClickedHandler}
-          weekNum={weekNum}
-          currentpage = {currentlyWeek-1}
-          record ={record}
-          WorkoutReducers={this.props.WorkoutReducers}
-        />
-      )
-    }
-    if(this.state.currentPage === 3){
-      RenderPage = (
-        <HistoryDetail 
-          exerciseData={exerciseData}
-         day={currentDay}
-         totalDays={totalDays}/>
-      )
-    }
+    // if(this.state.currentPage === 1){
+    //   RenderPage = (
+    //     <HistoryComponent
+    //       onListProgramClick={this.onListProgramClickHandler}
+         
+    //     />
+    //   )
+    // }
+    // if(this.state.currentPage === 2){
+    //   RenderPage = (
+    //     <HistoryWeekly 
+    //       onParticularDayClicked={this.onParticularDayClickedHandler}
+    //       weekNum={weekNum}
+    //       currentpage = {currentlyWeek-1}
+    //       record ={record}
+    //       WorkoutReducers={this.props.WorkoutReducers}
+    //     />
+    //   )
+    // }
+    // if(this.state.currentPage === 3){
+    //   RenderPage = (
+    //     <HistoryDetail 
+    //       exerciseData={exerciseData}
+    //       day={currentDay}
+    //       totalDays={totalDays}/>
+    //   )
+    // }
 
     return (
       <Hoc>
-        {RenderPage}
+        {/* {RenderPage} */}
         <div style={{margin:'auto'}}></div>
         <FooterContainer currentPath='history'/>
       </Hoc>
@@ -99,12 +91,14 @@ onParticularDayClickedHandler =(e, program ) => {
 function mapStateToProps(state){
   return {
     currentFooterTab: state.FooterReducers.currentFooterTab,
-    WorkoutReducers: state.WorkoutReducers
+    HistoryReducers: state.HistoryReducers
   }
 }
 function matchDispatchToProps(dispatch){
   return bindActionCreators({
-    selectFooter
+    selectFooter,
+    getProgram,
+    getExerciseRecord,
   }, dispatch
 );
 }
