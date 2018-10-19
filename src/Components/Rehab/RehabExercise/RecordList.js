c2eccc5457347461a88baa92145ffbbc69f40b44
XueYang <xueyangstudyz@gmail.com>
2018-10-19 12:49:02 +1100

    Rehab CSS optimisation

--- src/Components/Rehab/RehabExercise/RecordList.js
+++ src/Components/Rehab/RehabExercise/RecordList.js
@@ -8,35 +8,35 @@ const Item = List.Item
 const RecordList = (props) => {
   return(
     <div className="RepsBox">
-     {/* reps group Here */}
-     <div className="record-list">
-       <List>
-         {[...Array(4)].map((value, key) => {
-           return displayRecord(props, key);
-         })}
-       </List>
-     </div>
-     {/* complete butotn Here */}
+      {/* reps group Here */}
+      <div className="record-list">
+        <List>
+          {[...Array(4)].map((value, key) => {
+            return displayRecord(props, key);
+          })}
+        </List>
+      </div>
+      {/* complete butotn Here */}
 
-     <div className="completeButton">
-      <Button type="primary" inline size="small" onClick={()=> props.complete() }>
-        Complete
-      </Button>
-     </div>
+      <div className="completeButton">
+        <Button type="primary" inline onClick={()=> props.complete() }>
+          Complete
+        </Button>
+      </div>
     </div>
   )
 }
 
 const displayRecord =(props, index) => {
-    return(
-      <div className="list-records" key={index} >
-        <Item >
-          <Flex justify="between" className="list">
+  return(
+    <div className="list-records" key={index} >
+      <Item >
+        <Flex justify="between" className="list">
           <span className="list-number">{index+1}</span>
-          </Flex>
-        </Item>
-      </div>
-    )
+        </Flex>
+      </Item>
+    </div>
+  )
 
 }
 export default RecordList;
