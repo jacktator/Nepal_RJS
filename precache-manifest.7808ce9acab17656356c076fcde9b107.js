9a8eb0078c7c8025a5d5fc7b3f5f66e545844916
Yupeili <474680647@qq.com>
2019-01-03 14:22:05 +0800

    Updates

---
+++ precache-manifest.7808ce9acab17656356c076fcde9b107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "467ce6cb209bfa0fb00c",
+    "url": "/Nepal_RJS/static/js/runtime~main.467ce6cb.js"
+  },
+  {
+    "revision": "ea86b5aa93c896990b77",
+    "url": "/Nepal_RJS/static/js/main.ea86b5aa.chunk.js"
+  },
+  {
+    "revision": "0412559b9adf9db7c3c9",
+    "url": "/Nepal_RJS/static/js/1.0412559b.chunk.js"
+  },
+  {
+    "revision": "ea86b5aa93c896990b77",
+    "url": "/Nepal_RJS/static/css/main.a55979dc.chunk.css"
+  },
+  {
+    "revision": "781b8a796c181b393a457c7cd6f6d31f",
+    "url": "/Nepal_RJS/index.html"
+  }
+];
